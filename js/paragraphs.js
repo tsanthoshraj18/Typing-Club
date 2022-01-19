5763d5151565db44b79430d0e347e7f02b5671d4
tsanthoshraj18 <tsanthoshraj18@gmail.com>
2022-01-19 18:58:28 +0530

    first commit

---
+++ js/paragraphs.js
@@ -0,0 +1,3 @@
+const paragraphs = [
+   "Ulaanbaatar is the capital of Mongolia and its largest city.As a nomadic city,the capital used to move three times a year. With just four people per square mile, Mongolia is tht least densety populated country on Earth    . "
+];
